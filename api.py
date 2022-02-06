083a4bee53be596b393f42c2dc439f6ca6084263
Oisín <oinolan@tcd.ie>
2022-02-06 01:31:40 +0000

    Specified i/o types

--- api.py
+++ api.py
@@ -3,14 +3,24 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 class Inputs(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+    shop_buys: List[int]
+    food_weights: List[int]
+    food_prices: List[int]
+    shop_distances: List[int]
+    gas_price: int
+
+class Visit(BaseModel):
+    shop: int
+    crops: List[int]
+
+class Output(BaseModel):
+    routes: List[List[Visit]]
 
 app = FastAPI()
 
 @app.post("/compute_routes/")
 async def compute_routes(inputs: Inputs):
+    item: Output = f(...) # David's function goes here
     return item
